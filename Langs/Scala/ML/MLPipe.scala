98fb043a50fe069b22530f00f73e06d7786718cc
Kaique Gomes <br008095@gmail.com>
2022-09-13 09:26:38 -0300

    Adding comments

--- Langs/Scala/ML/MLPipe.scala
+++ Langs/Scala/ML/MLPipe.scala
@@ -1,11 +1,26 @@
 import org.apache.spark.sql.SparkSession
 
 import org.apache.log4j._
-//Logger.getLogger("org").setLevel(Level.ERROR)
+Logger.getLogger("org").setLevel(Level.WARN)
 
-val spark = SparkSessiom.builder().getOrCreate()
+// Creating the Spark Session
+val spark = SparkSession
+            .builder()
+            .getOrCreate()
 
-import org.apache.spark.ml.KMeans
+import org.apache.spark.ml.KMeans // Import the model from Spark ML
 
-val df = spark.read.option("header","true").option("inferSchema","true").csv("data.csv")
+// Reading the Data
+val df = spark.read
+        .option("header","true") // Reading the header
+        .option("inferSchema","true") // Inferring a data schema
+        .csv("data.csv") // Data Type
 
+// Selecting the columns to train model
+val feature_data = df.select($"Fresh",$"Milk",$"Grocery"
+                            ,$"Frozen",$"Detergents_Paper",$"Delicatessen")
+println(df.schema)
+
+// Importing models
+import org.apache.spark.ml.feature.{VectorAssembler,StringIndexer,VectorIndexer,OneHotEncoder}
+import org.apache.spark.ml.linalg.Vectors
