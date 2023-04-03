ebec0e4903aef4a3641df76e47c41d1a8dfb900b
Kaique Gomes <br008095@gmail.com>
2023-04-03 04:06:26 -0300

    Evolution and Solving problems

--- Docker/Airflow/dags/etl_job_v2.py
+++ Docker/Airflow/dags/etl_job_v2.py
@@ -5,43 +5,61 @@ import pendulum
 from datetime import timedelta
 import logging as log
 
-log.basicConfig(format='%(asctime)s | %(levelname)s | %(message)%',datefmt='%d - %M - %Y',level=log.INFO)
+log.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(message)%",
+    datefmt="%d - %M - %Y",
+    level=log.INFO
+)
 
-args_ = {
-    'owner': 'airflow'
-    ,'retries': 1
-    ,'retries_delay': timedelta(seconds=60)
-}
+args_ = {"owner": "airflow", "retries": 1, "retries_delay": timedelta(seconds=60)}
 
-@dag(dag_id='process_improved'
-         ,default_args=args_
-         ,start_date=pendulum.today().add(-1)
-        # ,schedule='0 13 * * *'
-        ,schedule='@once')
-    
+
+@dag(
+    dag_id="process_improved",
+    default_args=args_,
+    start_date=pendulum.today().add(-1)
+    # ,schedule='0 13 * * *'
+    ,
+    schedule="@once",
+)
 def main_job():
-    def extract_data():
+    def extract_data(**kwargs):
         import csv
-            
-        log.info('Starting extraction process')
-        data = []
-            
-        with open('/opt/airflow/dags/data/DIM_CLIENTE.csv','r') as f:
-            reader = csv.reader(f)
+
+        log.info("Starting extraction process")
+
+        with open("/opt/airflow/dags/data/DIM_CLIENTE.csv", "r") as f:
+            # reader = csv.reader(f) Fix Mapping problem
+            reader = csv.DictReader(f)
             for l in reader:
-                data.append(tuple(l))
-        return data
-    
-    ext_op = PythonOperator(task_id = 'extract_data',python_callable=extract_data)
-               
-    log.info('Inserting data on table of Postgres')
+                # data.append(tuple(l)) Fix Mapping problem
+                data = dict(l)
+        
+        log.info(data)
 
-    saving_data = PostgresOperator(
-        task_id = 'load_data',
-        sql = 'Insert into lab06.DIM_CLIENTE (id_cliente,nome_cliente,sobrenome_cliente) VALUES (%s,%s,%s)',
-        postgres_conn_id= 'DWFormation',
-        params = ext_op.python_callable()
+        log.info("Inserting data on table of Postgres")
+        
+        ddl = "Insert into lab06.DIM_CLIENTE (%s) VALUES (%s)" % (",".join(data.keys()), ",".join("%s" for _ in data.keys())) 
+        
+        p_operator = PostgresOperator(
+            task_id="load_data",
+            # sql = 'Insert into lab06.DIM_CLIENTE (id_cliente,nome_cliente,sobrenome_cliente) VALUES (%s,%s,%s)', Fix Mapping problem
+            sql=ddl,
+            postgres_conn_id="DWFormation",
+            # params = ext_op.python_callable() Fix Mapping problem
+            params=(data)
+        )
+        p_operator.execute(context=kwargs)
+
+    extAndsaving_data = PythonOperator(
+        task_id="extractAndsave_data",
+        python_callable=extract_data,
+        provide_context=True,
+        op_kwargs={
+            "params": {"csv_file_path": "/opt/airflow/dags/data/DIM_CLIENTE.csv"}
+        }
     )
-    saving_data
 
-test = main_job()
+    extAndsaving_data
+
+job = main_job()
