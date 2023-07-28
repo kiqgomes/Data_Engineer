d193d5c16d4bdb626e374b49534e3f6a0f701068
bdkiqdd <bdkiqdd>
2023-07-28 17:05:19 +0000

    secrets

--- Databricks/mount_datalake_azure.scala
+++ Databricks/mount_datalake_azure.scala
@@ -11,9 +11,9 @@ dbutils.fs.ls("/mnt")
 val configs = Map(
   "fs.azure.account.auth.type" -> "OAuth",
   "fs.azure.account.oauth.provider.type" -> "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-  "fs.azure.account.oauth2.client.id" -> "cb40893a-f218-4e92-a3f9-daec6772b754",
-  "fs.azure.account.oauth2.client.secret" -> "uKN8Q~HAv3osrjrjZcIX1wS-22ln.G~ZuWpzNbkh",
-  "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com/a3bf4c1e-d28c-4cbb-ba23-404a8c032b4a/oauth2/token")
+  "fs.azure.account.oauth2.client.id" -> "",
+  "fs.azure.account.oauth2.client.secret" -> "",
+  "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com//oauth2/token")
 dbutils.fs.mount(
   source = "abfss://base@datalakekiq.dfs.core.windows.net/",
   mountPoint = "/mnt/data",
